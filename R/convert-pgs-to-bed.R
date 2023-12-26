f7587f886b4274255b50b3bb0f4087f231023e18
Nicole Zeltser <nicole.zeltser@gmail.com>
2023-12-25 16:51:21 -0800

    add BED conversion and BED merge functions

---
+++ R/convert-pgs-to-bed.R
@@ -0,0 +1,94 @@
+### CONVERT.PGS.TO.BED ###################################################
+# convert imported and formatted PRS files to BED format
+# data = data.frame, SNP info with standardized CHROM and POS columns
+# chr.prefix = Bool, whether to add the 'chr' prefix to CHROM name
+# chr.X = Bool, whether to use 'X' for X-chromosome notation. If false, use numeric '23'.
+# slop = int, extra distance in bp to add to the interval on either side
+convert.pgs.to.bed <- function(data, chr.prefix = TRUE, chr.X = TRUE, slop = 0) {
+
+    ## standardize X chromosome notation as requested ##
+    # remove 'chr' strings from PRS CHROM by default
+	data$CHROM <- gsub('chr', '', data$CHROM);
+    # set to 'X' by default
+    data$CHROM <- gsub('23', 'X', data$CHROM);
+    # apply chr.X setting
+    if (!chr.X) {
+        data$CHROM <- gsub('X', '23', data$CHROM);
+        }
+
+	## standardize chromosome prefix as requested ##
+    # if chr.prefix is true, add 'chr' back to CHROM column
+    if (chr.prefix) {
+        data$CHROM <- paste0('chr', data$CHROM);
+        }
+
+    ## assemble BED file ##
+	# 0-index coordinates
+	prs.bed <- data.frame(
+		chr = data$CHROM,
+		start = data$POS - 1,
+		end = data$POS
+		);
+
+    # add slop
+    prs.bed$start <- prs.bed$start - slop;
+    prs.bed$end <- prs.bed$end + slop;
+
+	# concat with the rest of the prs columns
+	prs.bed <- cbind(prs.bed, subset(data, select = -c(CHROM, POS)));
+
+	return(prs.bed);
+	}
+
+### MERGE.PGS.BED ##################################################
+# pgs.bed.list = named list of pgs coordinates in BED format
+merge.pgs.bed <- function(pgs.bed.list, slop = 0) {
+
+    # check that pgs.bed.list is a named list
+    if (!is.list(pgs.bed.list) | is.null(names(pgs.bed.list))) {
+        stop('pgs.bed.list must be a named list');
+        }
+
+    # check that all elements of pgs.bed.list are data.frames
+    if (!all(sapply(pgs.bed.list, is.data.frame))) {
+        stop('all elements of pgs.bed.list must be data.frames');
+        }
+    
+    # check that all elements of pgs.bed.list have the same column names
+    if (!all(sapply(pgs.bed.list, function(x) all(colnames(x) == colnames(pgs.bed.list[[1]]))))) {
+        stop('all elements of pgs.bed.list must have the same column names');
+        }
+    
+    # check that each element of pgs.bed.list is in BED format (chr, start, end)
+    if (!all(sapply(pgs.bed.list, function(x) all(colnames(x) %in% c('chr', 'start', 'end'))))) {
+        stop('all elements of pgs.bed.list must have columns named chr, start, and end');
+        }
+    
+    # check that all intervals specified in pgs.bed.list are one bp in length
+    if (!all(sapply(pgs.bed.list, function(x) all(x$start == x$end - 1)))) {
+        stop('all intervals specified in pgs.bed.list must represent one SNP and be one bp in length');
+        }
+
+    # Annotate each PGS BED file with the name of the PGS
+    for (i in 1:length(pgs.bed.list)) {
+        pgs.bed.list[[i]]$annotation <- names(pgs.bed.list)[i];
+        }
+
+    # concatenate all BED files in list
+    concatenated.bed <- do.call(rbind, pgs.bed.list);
+
+    # add snpID column to annotation
+    concatenated.bed$annotation <- paste(concatenated.bed$rsID, concatenated.bed$annotation, sep = '|');
+
+    # sort by chromosome and position
+    concatenated.bed <- concatenated.bed[order(concatenated.bed$chr, concatenated.bed$start), ];
+
+    # merge overlapping SNP intervals and combine overlapping annotations using aggregate
+    merged.bed <- aggregate(annotation ~ chr + start + end, data = concatenated.bed, FUN = paste, collapse = ',');
+
+    # sort by chromosome and position
+    merged.bed <- merged.bed[order(merged.bed$chr, merged.bed$start), ];
+
+    # return merged BED file
+    return(merged.bed);
+    }
