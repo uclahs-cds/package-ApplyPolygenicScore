274c3df38ea76e8332c513aa1ac063753678b0d8
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-01-08 15:51:28 -0800

    start test file for mergeing bed files

---
+++ tests/testthat/test-bed-merge.R
@@ -0,0 +1,105 @@
+test_that(
+    'merge.pgs.bed correctly checks input', {
+        # check that pgs.bed.list is a named list
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = 'not.a.named.list'),
+            'pgs.bed.list must be a named list'
+            );
+        
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = list(
+                data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4)
+                    ),
+            'pgs.bed.list must be a named list'
+            ))
+        )
+
+        # check that pgs.bed.list is a list of data.frames
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = list(
+                test = 'not.a.data.frame'
+                )),
+            'all elements of pgs.bed.list must be data.frames'
+            );
+
+        # check that all elements of pgs.bed.list have the same column names
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = list(
+                name1 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4),
+                    foo = c(1, 2, 3)
+                    ),
+                name2 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4)
+                    )
+                )),
+            'all elements of pgs.bed.list must have the same column names'
+            );
+
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = list(
+                name1 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4),
+                    foo = c(1, 2, 3)
+                    ),
+                name2 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4),
+                    bar = c(1, 2, 3)
+                    )
+                )),
+            'all elements of pgs.bed.list must have the same column names'
+            );
+
+
+        # check that pgs.bed.list has required columns
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = list(
+                name1 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    foo = c(1, 2, 3)
+                    )
+                )),
+            'all elements of pgs.bed.list must have columns named chr, start, and end'
+            );
+
+        # check that provided coordinates are one bp in length
+        expect_error(
+            merge.pgs.bed(pgs.bed.list = list(
+                name1 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(3, 4, 5)
+                    )
+                )),
+            'all intervals specified in pgs.bed.list must represent one SNP and be one bp in length'
+            );
+
+        # check that correct input is accepted
+        expect_silent(
+            merge.pgs.bed(pgs.bed.list = list(
+                name1 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4)
+                    ),
+                name2 = data.frame(
+                    chr = c('1', '2', '3'),
+                    start = c(1, 2, 3),
+                    end = c(2, 3, 4)
+                    )
+                ))
+            );
+        }
+    );
