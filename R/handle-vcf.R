fc579963d49a6875c00bd83b1df191f1c4f85159
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-01-11 18:30:06 -0800

    add documentation

--- R/handle-vcf.R
+++ R/handle-vcf.R
@@ -1,7 +1,6 @@
-### GET.LONG.FORM.VCF.VCFR ########################################################
-# Wrapper for vcfR2tidy which accounts for the bug in this function that prevents
-# processing of VCF files with empty INFO columns.
-get.long.form.vcf.vcfR <- function(vcf.vcfR, info.fields, format.fields) {
+# A utility wrapper function for vcfR::vcfR2tidy() that handles a bug in vcfR
+# that causes an error when the VCF file contains no INFO fields.
+get.long.form.vcf.vcfR <- function(vcf.vcfR, info.fields = NULL, format.fields = NULL) {
   # check for vcfR empty INFO bug:
   INFO.meta.lines <- grepl('^##INFO', vcf.vcfR@meta);
   if (any(!INFO.meta.lines)) {
@@ -10,7 +9,7 @@ get.long.form.vcf.vcfR <- function(vcf.vcfR, info.fields, format.fields) {
       }
 
   # convert to long form dataframe w/ vcfR
-  long.vcf <- vcfR2tidy(
+  long.vcf <- vcfR::vcfR2tidy(
       x = vcf.vcfR,
       single_frame = TRUE,
       info_fields = info.fields,
@@ -20,15 +19,23 @@ get.long.form.vcf.vcfR <- function(vcf.vcfR, info.fields, format.fields) {
   return(long.vcf);
   }
 
-import.vcf <- function(input, info.fields, format.fields, verbose = FALSE) {
+#' @title Import VCF file
+#' @description Import a VCF file into a long form data.frame using vcfR package.
+#' @param vcf.path A character string indicating the path to the VCF file to be imported.
+#' @param info.fields A character vector indicating the INFO fields to be imported.
+#' @param format.fields A character vector indicating the FORMAT fields to be imported.
+#' @param verbose A logical indicating whether verbose output should be printed by vcfR.
+#' @return A long form data.frame containing the VCF data.
+#' @export
+import.vcf <- function(vcf.path, info.fields = NULL, format.fields = NULL, verbose = FALSE) {
   # import VCF file with vcfR
-  vcf.vcfR <- vcfR::read.vcfR(input, convertNA = TRUE, verbose = verbose);
+  vcf.vcfR <- vcfR::read.vcfR(file = vcf.path, convertNA = TRUE, verbose = verbose);
 
   # convert to long form dataframe w/ vcfR
   long.vcf <- get.long.form.vcf.vcfR(
       vcf.vcfR = vcf.vcfR,
       info.fields = info.fields,
-      format.fields = format.fields,
+      format.fields = format.fields
       );
 
   return(long.vcf);
