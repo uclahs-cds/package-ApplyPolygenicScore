9ed0ee63dc418602b58012559d832958068bda2e
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-01-13 16:10:54 -0800

    refactor vcfR empty INFO bug handling

--- R/handle-vcf.R
+++ R/handle-vcf.R
@@ -1,23 +1,16 @@
-# A utility wrapper function for vcfR::vcfR2tidy() that handles a bug in vcfR
+# A utility function for vcfR::vcfR2tidy() that handles a bug in vcfR
 # that causes an error when the VCF file contains no INFO fields.
-get.long.form.vcf.vcfR <- function(vcf.vcfR, info.fields = NULL, format.fields = NULL) {
-  # check for vcfR empty INFO bug:
-  INFO.meta.lines <- grepl('^##INFO', vcf.vcfR@meta);
-  if (any(!INFO.meta.lines)) {
-      dummy.INFO.line <- '##INFO=<ID=AF>';
-      vcf.vcfR@meta[length(vcf.vcfR@meta) + 1] <- dummy.INFO.line;
-      }
+check.for.no.info.fields <- function(vcf.vcfR) {
+    # If no INFO fields present, add dummy INFO line
+    INFO.meta.lines <- grepl('^##INFO', vcf.vcfR@meta);
+    if (!any(INFO.meta.lines)) {
+            dummy.INFO.line <- '##INFO=<ID=DUMMY>';
+            vcf.vcfR@meta[length(vcf.vcfR@meta) + 1] <- dummy.INFO.line;
+            warning('No INFO fields present in VCF file. Adding dummy INFO field "DUMMY" to avoid vcfR import errors.')
+            }
 
-  # convert to long form dataframe w/ vcfR
-  long.vcf <- vcfR::vcfR2tidy(
-      x = vcf.vcfR,
-      single_frame = TRUE,
-      info_fields = info.fields,
-      format_fields = format.fields,
-      );
-
-  return(long.vcf);
-  }
+    return(vcf.vcfR);
+    }
 
 #' @title Import VCF file
 #' @description Import a VCF file into a long form data.frame using vcfR package.
@@ -25,18 +18,22 @@ get.long.form.vcf.vcfR <- function(vcf.vcfR, info.fields = NULL, format.fields =
 #' @param info.fields A character vector indicating the INFO fields to be imported.
 #' @param format.fields A character vector indicating the FORMAT fields to be imported.
 #' @param verbose A logical indicating whether verbose output should be printed by vcfR.
-#' @return A long form data.frame containing the VCF data.
+#' @return A list containing a tibble of VCF meta data and a tibble data.frame containing the parsed VCF data in long form.
 #' @export
 import.vcf <- function(vcf.path, info.fields = NULL, format.fields = NULL, verbose = FALSE) {
-  # import VCF file with vcfR
-  vcf.vcfR <- vcfR::read.vcfR(file = vcf.path, convertNA = TRUE, verbose = verbose);
+    # import VCF file with vcfR
+    vcf.vcfR <- vcfR::read.vcfR(file = vcf.path, convertNA = TRUE, verbose = verbose);
 
-  # convert to long form dataframe w/ vcfR
-  long.vcf <- get.long.form.vcf.vcfR(
-      vcf.vcfR = vcf.vcfR,
-      info.fields = info.fields,
-      format.fields = format.fields
-      );
+    # check for no INFO fields vcfR bug
+    vcf.vcfR <- check.for.no.info.fields(vcf.vcfR);
 
-  return(long.vcf);
-  }
+    # convert to long form tibble dataframe w/ vcfR
+    long.vcf <- vcfR::vcfR2tidy(
+        x = vcf.vcfR,
+        single_frame = TRUE,
+        info_fields = info.fields,
+        format_fields = format.fields
+        );
+
+    return(long.vcf);
+    }
