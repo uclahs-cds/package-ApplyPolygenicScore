1383ce296f2b6ab95461331ef7e9e63fcae1face
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-01-19 21:48:12 -0800

    start pgs calc function

--- R/apply-pgs.R
+++ R/apply-pgs.R
@@ -63,3 +63,37 @@ merge.vcf.with.pgs <- function(vcf.data, pgs.weight.data) {
     return(output);
     }
 
+apply.polygenic.score <- function(vcf.data, pgs.weight.data) {
+    # check that inputs are data.frames
+    if (!is.data.frame(vcf.data)) {
+        stop('vcf.data must be a data.frame');
+        }
+    if (!is.data.frame(pgs.weight.data)) {
+        stop('pgs.weight.data must be a data.frame');
+        }
+
+    # check that inputs contain required columns for PGS application
+    required.vcf.columns <- c('CHROM', 'POS', 'REF', 'ALT', 'Indiv', 'gt_GT_alleles');
+    required.pgs.columns <- c('CHROM', 'POS', 'effect_allele', 'beta');
+    if (!all(required.vcf.columns %in% colnames(vcf.data))) {
+        stop('vcf.data must contain columns named CHROM, POS, REF, ALT, Indiv, and gt_GT_alleles');
+        }
+    if (!all(required.pgs.columns %in% colnames(pgs.weight.data))) {
+        stop('pgs.weight.data must contain columns named CHROM, POS, effect_allele, and beta');
+        }
+
+    # check that VCF data is in long form
+    n.samples <- length(unique(vcf.data$Indiv));
+    n.variants <- length(unique(paste0(vcf.data$CHROM, vcf.data$POS)));
+    if (nrow(vcf.data) != n.samples * n.variants) {
+        stop('vcf.data is not in long form. Please use vcfR::vcfR2tidy() to convert to long form.');
+        }
+
+    # merge VCF and PGS data
+    merged.vcf.with.pgs <- merge.vcf.with.pgs(
+        vcf.data = vcf.data,
+        pgs.weight.data = pgs.weight.data
+        );
+
+    return(merged.vcf.with.pgs);
+}
