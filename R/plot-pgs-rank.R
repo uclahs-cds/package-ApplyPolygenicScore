8ccaf2f220de9b12c165beae9b46cde3319a7456
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-03-30 18:58:04 -0700

    percentile covariate data prep

--- R/plot-pgs-rank.R
+++ R/plot-pgs-rank.R
@@ -1,3 +1,19 @@
+# utility function for color handling
+# Given a vector of feature names and given a named color scheme
+# (named vector of colors with names being feature names)
+# return a vector of colors corresponding to each feature.
+create.feature.color.vector <- function(features, color.scheme) {
+  # Replace features with corresponding color
+  feature.colors <- sapply(
+    X = features,
+    FUN = function(feature) {
+        color.scheme[feature]
+        }
+    );
+  names(feature.colors) <- features;
+  return(feature.colors);
+  }
+
 rank.plotting.input.checks <- function(pgs.data, phenotype.columns) {
     # check pgs.data
     if (!is.data.frame(pgs.data)) {
@@ -78,5 +94,44 @@ plot.pgs.rank <- function(
         );
 
     # percentile covariate bars
+
+    # quantile color schemes
+    decile.color.scheme <- c(
+        paste0('grey', seq(1,100, length.out = 10))
+        );
+    names(decile.color.scheme) <- as.character(1:10);
+
+    quartile.color.scheme <- c(
+        decile.color.scheme[seq(1, 10, length.out = 4)]
+        );
+    names(quartile.color.scheme) <- as.character(1:4);
+
+    if ('n.percentile' %in% colnames(pgs.data)) {
+        percentile.color.scheme <- c(
+            paste0('grey', round(seq(1,100, length.out = length(unique(pgs.data$n.percentile)))))
+            );
+        names(percentile.color.scheme) <- as.character(unique(pgs.data$n.percentile));
+        percentile.color.scheme.list <- list(decile = decile.color.scheme, quartile = quartile.color.scheme, n.percentile = percentile.color.scheme);
+        percentile.covariate.data <- subset(pgs.data, select = c('decile', 'quartile', 'n.percentile'));
+        } else {
+            percentile.covariate.data <- subset(pgs.data, select = c('decile', 'quartile'));
+            percentile.color.scheme.list <- list(decile = decile.color.scheme, quartile = quartile.color.scheme);
+            }
+
+    percentile.covariate.df <- sapply(
+        X = colnames(percentile.covariate.data),#1:length(percentile.covariate.data),
+        FUN = function(x) {
+            covariate.values <- percentile.covariate.data[ , x];
+            color.vector <- create.feature.color.vector(
+                features = covariate.values,
+                color.scheme = percentile.color.scheme.list[[x]]
+                );
+            return(color.vector)
+            }
+        );
+    percentile.covariate.df <- data.frame(t(percentile.covariate.df));
+    colnames(percentile.covariate.df) <- pgs.data$Indiv;
+    # order by percentile
+    percentile.covariate.df <- percentile.covariate.df[ , order(pgs.data$percentile)];
 
     }
