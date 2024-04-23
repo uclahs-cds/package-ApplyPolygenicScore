107d7b37b7a0ece662ff24de2cb41d1442023811
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-04-23 13:21:56 -0700

    lintr fix

--- R/data/data.R
+++ R/data/data.R
@@ -1,5 +1,5 @@
 #' Genome In a Bottle (GIAB) VCF formatted data.
-#' 
+#'
 #' A subset of the genotype callset provided by the GIAB consortium for public use as a benchmarking dataset.
 #' These files contain genotype information from the NA12878/HG001 HapMap individual.
 #' These variants were aligned and called against the GRCh38 reference genome.
@@ -8,11 +8,11 @@
 #' plus 10bp of flanking sequence on either side of the variant.
 #' Typically, a cohort intended for polygenic scoring is combined into one VCF, thus we have added a second sample to the VCF by
 #' merging the single-sample GIAB VCF with itself, producing genotype data for HG001 and its duplicate HG001.2.
-#' 
+#'
 #' @format ## `*_GIAB.vcf.gz`
 #' g-zipped VCF file containing 234 lines of metadata in the header indicated by `##`, one line of column header
 #' indicated by `#` and 63 lines of genetic information. The VCF has 9 standard columns and 1 or 2 additional sample columns.
-#' 
+#'
 #' \describe{
 #'  \item{CHROM}{Chromosome}
     #' \item{POS}{Position}
