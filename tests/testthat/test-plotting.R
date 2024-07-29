3fdd7b5630e92fdd53c2c0c49af53851b75b85ff
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-07-29 13:58:30 -0700

    update tests

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,5 +1,5 @@
 # plotting functions take a long time to run, this var toggles off plotting tests for faster testing
-SKIP.PLOTS <- FALSE;
+SKIP.PLOTS <- TRUE;
 SKIP.COMPREHENSIVE.CASES <- FALSE;
 skip.plotting.tests <- function(skip.plots = FALSE) {
     if (skip.plots) {
--- tests/testthat/test-vcf-pgs-merge.R
+++ tests/testthat/test-vcf-pgs-merge.R
@@ -1,5 +1,5 @@
 test_that(
-    'merge.vcf.with.pgs correctly checks inputs', {
+    'combine.vcf.with.pgs correctly checks inputs', {
         test.vcf.data <- data.frame(
             CHROM = c('chr1', 'chr1', 'chr1', 'chr1'),
             POS = c(1, 2, 3, 4),
@@ -15,14 +15,14 @@ test_that(
 
         # check that inputs are data.frames
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = 'not a data.frame',
                 pgs.weight.data = test.pgs.weight.data
                 ),
             'vcf.data must be a data.frame'
             );
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data,
                 pgs.weight.data = 'not a data.frame'
                 ),
@@ -31,7 +31,7 @@ test_that(
 
         # check required columns
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = data.frame(
                     CHROM = c('chr1', 'chr1', 'chr1', 'chr1'),
                     foo = c(1, 2, 3, 4),
@@ -43,7 +43,7 @@ test_that(
             'vcf.data must contain columns named CHROM and POS'
             );
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = data.frame(
                     foo = c('chr1', 'chr1', 'chr1', 'chr1'),
                     POS = c(1, 2, 3, 4),
@@ -55,7 +55,7 @@ test_that(
             'vcf.data must contain columns named CHROM and POS'
             );
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data,
                 pgs.weight.data = data.frame(
                     CHROM = c('chr1', 'chr1', 'chr1', 'chr1'),
@@ -67,7 +67,7 @@ test_that(
             'pgs.weight.data must contain columns named CHROM and POS'
             );
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data,
                 pgs.weight.data = data.frame(
                     foo = c('chr1', 'chr1', 'chr1', 'chr1'),
@@ -82,7 +82,7 @@ test_that(
     )
 
 test_that(
-    'merge.vcf.with.pgs stops when all PGS SNPs are missing from the VCF', {
+    'combine.vcf.with.pgs stops when all PGS SNPs are missing from the VCF', {
         test.vcf.data <- data.frame(
             CHROM = c('chr1', 'chr1', 'chr1', 'chr1'),
             POS = c(1, 2, 3, 4),
@@ -97,7 +97,7 @@ test_that(
             );
 
         expect_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data,
                 pgs.weight.data = test.pgs.weight.data
                 ),
@@ -107,7 +107,7 @@ test_that(
     );
 
 test_that(
-    'merge.vcf.with.pgs correctly formats CHROM column', {
+    'combine.vcf.with.pgs correctly formats CHROM column', {
 
         test.vcf.data <- data.frame(
             CHROM = c('chr1', 'chr1', 'chr1', 'chr1'),
@@ -122,7 +122,7 @@ test_that(
             beta = c(1, 2, 3, 4)
             );
 
-        chr.prefix.vcf.no.prefix.pgs <- merge.vcf.with.pgs(
+        chr.prefix.vcf.no.prefix.pgs <- combine.vcf.with.pgs(
             vcf.data = test.vcf.data,
             pgs.weight.data = data.frame(
                 CHROM = c('1', '1', '1', '1'),
@@ -131,7 +131,7 @@ test_that(
                 beta = c(1, 2, 3, 4)
                 )
             )$merged.vcf.with.pgs.data;
-        no.prefix.vcf.chr.prefix.pgs <- merge.vcf.with.pgs(
+        no.prefix.vcf.chr.prefix.pgs <- combine.vcf.with.pgs(
             vcf.data = data.frame(
                 CHROM = c('1', '1', '1', '1'),
                 POS = c(1, 2, 3, 4),
@@ -140,7 +140,7 @@ test_that(
                 ),
                 pgs.weight.data = test.pgs.weight.data
             )$merged.vcf.with.pgs.data;
-        numeric.sex.vcf.alpha.sex.pgs <- merge.vcf.with.pgs(
+        numeric.sex.vcf.alpha.sex.pgs <- combine.vcf.with.pgs(
             vcf.data = data.frame(
                 CHROM = c('23', '23', '24', '24'),
                 POS = c(1, 2, 3, 4),
@@ -154,7 +154,7 @@ test_that(
                     beta = c(1, 2, 3, 4)
                     )
             )$merged.vcf.with.pgs.data;
-        alpha.sex.vcf.numeric.sex.pgs <- merge.vcf.with.pgs(
+        alpha.sex.vcf.numeric.sex.pgs <- combine.vcf.with.pgs(
             vcf.data = data.frame(
                 CHROM = c('X', 'X', 'Y', 'Y'),
                 POS = c(1, 2, 3, 4),
@@ -189,7 +189,7 @@ test_that(
     );
 
 test_that(
-    'merge.vcf.with.pgs correctly formats output', {
+    'combine.vcf.with.pgs correctly formats output', {
         test.vcf.data <- data.frame(
             CHROM = c('chr1', 'chr1', 'chr1', 'chr1'),
             POS = c(1, 2, 3, 4),
@@ -207,32 +207,32 @@ test_that(
 
         # check that no error is thrown when inputs are correct
         expect_no_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data,
                 pgs.weight.data = test.pgs.weight.data
                 )
             );
 
-        test.merge.vcf.with.pgs <- merge.vcf.with.pgs(
+        test.combine.vcf.with.pgs <- combine.vcf.with.pgs(
             vcf.data = test.vcf.data,
             pgs.weight.data = test.pgs.weight.data
             );
 
-        # check that merge.vcf.with.pgs returns a list
+        # check that combine.vcf.with.pgs returns a list
         expect_type(
-            test.merge.vcf.with.pgs,
+            test.combine.vcf.with.pgs,
             'list'
             );
 
-        # check that merge.vcf.with.pgs returns a data.frame
+        # check that combine.vcf.with.pgs returns a data.frame
         expect_s3_class(
-            test.merge.vcf.with.pgs$merged.vcf.with.pgs.data,
+            test.combine.vcf.with.pgs$merged.vcf.with.pgs.data,
             'data.frame'
             );
 
-        # check that merge.vcf.with.pgs returns a data.frame
+        # check that combine.vcf.with.pgs returns a data.frame
         expect_s3_class(
-            test.merge.vcf.with.pgs$missing.snp.data,
+            test.combine.vcf.with.pgs$missing.snp.data,
             'data.frame'
             );
 
@@ -240,7 +240,7 @@ test_that(
     );
 
 test_that(
-    'merge.vcf.with.pgs correctly performs merge', {
+    'combine.vcf.with.pgs correctly performs merge', {
         test.vcf.data.no.missing <- data.frame(
             CHROM = c('chr1', 'chr2', 'chr3', 'chr4'),
             POS = c(1, 2, 3, 4),
@@ -264,117 +264,117 @@ test_that(
             beta = c(1, 2, 3, 4)
             );
 
-        # check that merge.vcf.with.pgs throws a warning for missing SNPs
+        # check that combine.vcf.with.pgs throws a warning for missing SNPs
         expect_warning(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data.missing,
                 pgs.weight.data = test.pgs.weight.data
                 ),
             'PGS is missing 1 SNPs from VCF'
             );
 
-        test.merge.vcf.with.pgs.no.missing <- merge.vcf.with.pgs(
+        test.combine.vcf.with.pgs.no.missing <- combine.vcf.with.pgs(
             vcf.data = test.vcf.data.no.missing,
             pgs.weight.data = test.pgs.weight.data
             );
 
-        test.merge.vcf.with.pgs.missing <- merge.vcf.with.pgs(
+        test.combine.vcf.with.pgs.missing <- combine.vcf.with.pgs(
             vcf.data = test.vcf.data.missing,
             pgs.weight.data = test.pgs.weight.data
             );
 
-        # check that merge.vcf.with.pgs returns the correct number of rows
+        # check that combine.vcf.with.pgs returns the correct number of rows
         expect_equal(
-            nrow(test.merge.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data),
+            nrow(test.combine.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data),
             4
             );
         expect_equal(
-            nrow(test.merge.vcf.with.pgs.missing$merged.vcf.with.pgs.data),
+            nrow(test.combine.vcf.with.pgs.missing$merged.vcf.with.pgs.data),
             4
             );
         expect_equal(
-            nrow(test.merge.vcf.with.pgs.missing$missing.snp.data),
+            nrow(test.combine.vcf.with.pgs.missing$missing.snp.data),
             1
             );
         expect_equal(
-            test.merge.vcf.with.pgs.no.missing$missing.snp.data,
+            test.combine.vcf.with.pgs.no.missing$missing.snp.data,
             NULL
             );
 
-        # check that merge.vcf.with.pgs returns the correct number of columns
+        # check that combine.vcf.with.pgs returns the correct number of columns
         expect_equal(
-            ncol(test.merge.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data),
+            ncol(test.combine.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data),
             8
             );
         expect_equal(
-            ncol(test.merge.vcf.with.pgs.missing$merged.vcf.with.pgs.data),
+            ncol(test.combine.vcf.with.pgs.missing$merged.vcf.with.pgs.data),
             8
             );
 
-        # check that merge.vcf.with.pgs returns the correct columns
+        # check that combine.vcf.with.pgs returns the correct columns
         expect_true(
-            all(colnames(test.merge.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data) %in% c('CHROM', 'POS', 'REF', 'ALT', 'Indiv', 'gt_GT_alleles', 'effect_allele', 'beta'))
+            all(colnames(test.combine.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data) %in% c('CHROM', 'POS', 'REF', 'ALT', 'Indiv', 'gt_GT_alleles', 'effect_allele', 'beta'))
             );
         expect_true(
-            all(colnames(test.merge.vcf.with.pgs.missing$merged.vcf.with.pgs.data) %in% c('CHROM', 'POS', 'REF', 'ALT', 'Indiv', 'gt_GT_alleles', 'effect_allele', 'beta'))
+            all(colnames(test.combine.vcf.with.pgs.missing$merged.vcf.with.pgs.data) %in% c('CHROM', 'POS', 'REF', 'ALT', 'Indiv', 'gt_GT_alleles', 'effect_allele', 'beta'))
             );
 
-        # check that merge.vcf.with.pgs returns the correct values
+        # check that combine.vcf.with.pgs returns the correct values
         expect_equal(
-            test.merge.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data$CHROM,
+            test.combine.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data$CHROM,
             c('chr1', 'chr2', 'chr3', 'chr4')
             );
         expect_equal(
-            test.merge.vcf.with.pgs.missing$merged.vcf.with.pgs.data$CHROM,
+            test.combine.vcf.with.pgs.missing$merged.vcf.with.pgs.data$CHROM,
             c('chr1', 'chr2', 'chr3', 'chr4')
             );
         expect_equal(
-            test.merge.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data$POS,
+            test.combine.vcf.with.pgs.no.missing$merged.vcf.with.pgs.data$POS,
             c(1, 2, 3, 4)
             );
         expect_equal(
-            test.merge.vcf.with.pgs.missing$merged.vcf.with.pgs.data$POS,
+            test.combine.vcf.with.pgs.missing$merged.vcf.with.pgs.data$POS,
             c(1, 2, 3, 4)
             );
-        # check that merge.vcf.with.pgs returns the correct values for missing SNPs
+        # check that combine.vcf.with.pgs returns the correct values for missing SNPs
         expect_equal(
-            test.merge.vcf.with.pgs.missing$missing.snp.data$CHROM,
+            test.combine.vcf.with.pgs.missing$missing.snp.data$CHROM,
             'chr4'
             );
         expect_equal(
-            test.merge.vcf.with.pgs.missing$missing.snp.data$POS,
+            test.combine.vcf.with.pgs.missing$missing.snp.data$POS,
             4
             );
         expect_true(
-            is.na(test.merge.vcf.with.pgs.missing$missing.snp.data$REF)
+            is.na(test.combine.vcf.with.pgs.missing$missing.snp.data$REF)
             );
         expect_true(
-            is.na(test.merge.vcf.with.pgs.missing$missing.snp.data$ALT)
+            is.na(test.combine.vcf.with.pgs.missing$missing.snp.data$ALT)
             );
         expect_true(
-            is.na(test.merge.vcf.with.pgs.missing$missing.snp.data$Indiv)
+            is.na(test.combine.vcf.with.pgs.missing$missing.snp.data$Indiv)
             );
         expect_true(
-            is.na(test.merge.vcf.with.pgs.missing$missing.snp.data$gt_GT_alleles)
+            is.na(test.combine.vcf.with.pgs.missing$missing.snp.data$gt_GT_alleles)
             );
         expect_equal(
-            test.merge.vcf.with.pgs.missing$missing.snp.data$effect_allele,
+            test.combine.vcf.with.pgs.missing$missing.snp.data$effect_allele,
             'G'
             );
         expect_equal(
-            test.merge.vcf.with.pgs.missing$missing.snp.data$beta,
+            test.combine.vcf.with.pgs.missing$missing.snp.data$beta,
             4
             );
         }
     );
 
 test_that(
-    'merge.vcf.with.pgs works correctly with real data', {
+    'combine.vcf.with.pgs works correctly with real data', {
         test.vcf.data <- import.vcf('data/HG001_GIAB.vcf.gz');
         test.pgs.weight.data <- import.pgs.weight.file('data/PGS003378_hmPOS_GRCh38.txt');
 
         expect_no_error(
-            merge.vcf.with.pgs(
+            combine.vcf.with.pgs(
                 vcf.data = test.vcf.data$dat,
                 pgs.weight.data = test.pgs.weight.data$pgs.weight.data
                 )
