dcbffd9925eee37702b4d7df9879384dac4b00d5
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-08-21 12:30:51 -0700

    add asterisk to allowed allele list

--- R/calculate-dosage.R
+++ R/calculate-dosage.R
@@ -25,7 +25,8 @@ convert.alleles.to.pgs.dosage <- function(called.alleles, risk.alleles) {
         split.alleles <- data.frame(called.alleles, called.alleles);
         } else {
             # check that called.alleles is a vector of genotypes in allelic notation or '.' separated by a slash or pipe
-            allowed.pattern <- '^((([A-Z]+|\\.)[/\\|]([A-Z]+|\\.))|\\.)$' # '|' are special chars in regular expressions
+            # "*" characters represent overlapping deletions from an upstream indel and are accepted VCF format
+            allowed.pattern <- '^((([A-Z]+|\\.|\\*)[/\\|]([A-Z]+|\\.|\\*))|\\.)$' # '|' are special chars in regular expressions
             passing.alleles <- grepl(allowed.pattern, called.alleles);
             passing.alleles[is.na(called.alleles)] <- TRUE; # NA allowed
             if (!all(passing.alleles)) {
