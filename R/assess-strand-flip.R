ae182a4334b1a40dcf22631c10f9c94ab122d062
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-09-25 16:34:57 -0700

    start strand flip functions

---
+++ R/assess-strand-flip.R
@@ -0,0 +1,104 @@
+flip.DNA.allele <- function(allele) {
+    if (all(is.na(allele))) {
+        return(NA);
+        }
+    # check that allele is a character vector
+    if (!is.character(allele)) {
+        stop('allele must be a character vector.');
+        }
+
+    accepted.alleles <- c('A', 'T', 'C', 'G');
+    allele.check <- allele[!is.na(allele)] %in% accepted.alleles;
+    if (any(!allele.check, na.rm = TRUE)) {
+        stop('Invalid allele: ', allele[!is.na(allele)][!allele.check]);
+        }
+
+    flipped.allele <- sapply(
+        X = allele,
+        FUN = switch,
+        'A' = 'T',
+        'T' = 'A',
+        'C' = 'G',
+        'G' = 'C',
+        `NA` = NA
+            );
+    return(flipped.allele);
+    }
+
+assess.strand.flip <- function(
+    vcf.ref.allele,
+    vcf.alt.allele,
+    pgs.ref.allele,
+    pgs.effect.allele
+    ) {
+
+    # initialize empty vectors to store flip info
+    flipped.effect.allele <- rep(NA, length(vcf.ref.allele));
+    flipped.other.allele <- rep(NA, length(vcf.ref.allele));
+    # strand status
+    flip.designation <- rep(NA, length(vcf.ref.allele));
+
+    for (i in 1:length(vcf.ref.allele)) {
+        # check if default ref-ref / alt-effect alleles match
+        default.ref.check <- vcf.ref.allele[i] == pgs.ref.allele[i];
+        default.alt.check <- vcf.alt.allele[i] == pgs.effect.allele[i];
+
+        if(default.ref.check & default.alt.check) {
+            flip.designation[i] <- 'default';
+            flipped.effect.allele[i] <- pgs.effect.allele[i];
+            flipped.other.allele[i] <- pgs.ref.allele[i];
+            break;
+            }
+
+        # check if effect designation is on the reference allele
+        effect.switch.ref.check <- vcf.ref.allele[i] == pgs.effect.allele[i];
+        effect.switch.alt.check <- vcf.alt.allele[i] == pgs.ref.allele[i];
+
+        if(effect.switch.ref.check & effect.switch.alt.check) {
+            flip.designation[i] <- 'effect_switch';
+            flipped.effect.allele[i] <- pgs.effect.allele[i];
+            flipped.other.allele[i] <- pgs.ref.allele[i];
+            break;
+            }
+
+        # attempt strand flip on default ref-ref / alt-effect alleles
+        pgs.ref.flip <- flip.DNA.allele(pgs.ref.allele[i]);
+        pgs.effect.flip <- flip.DNA.allele(pgs.effect.allele[i]);
+
+        default.ref.flip.check <- vcf.ref.allele[i] == pgs.ref.flip;
+        default.alt.flip.check <- vcf.alt.allele[i] == pgs.effect.flip;
+
+        if(default.ref.flip.check & default.alt.flip.check) {
+            flip.designation[i] <- 'strand_flip';
+            flipped.effect.allele[i] <- pgs.effect.flip;
+            flipped.other.allele[i] <- pgs.ref.flip;
+            break;
+            }
+
+        # attempt strand flip on effect_switch ref-effect / alt-ref alleles
+        effect.switch.ref.flip.check <- vcf.ref.allele[i] == pgs.effect.flip;
+        effect.switch.alt.flip.check <- vcf.alt.allele[i] == pgs.ref.flip;
+
+        if(effect.switch.ref.flip.check & effect.switch.alt.flip.check) {
+            flip.designation[i] <- 'effect_switch_with_strand_flip';
+            flipped.effect.allele[i] <- pgs.effect.flip;
+            flipped.other.allele[i] <- pgs.ref.flip;
+            break;
+            }
+
+        # if no match is found, declare ambiguous and return NA
+        flip.designation[i] <- 'ambiguous';
+        flipped.effect.allele[i] <- NA;
+        flipped.other.allele[i] <- NA;
+
+        }
+
+    return(
+        list(
+            strand.status = flip.designation,
+            new.pgs.effect.allele = flipped.effect.allele,
+            new.pgs.other.allele = flipped.other.allele
+            )
+        );
+
+    }
