57263835b9e010cfe29d56e85eb71b69c11d00ae
Nicole Zeltser <nicole.zeltser@gmail.com>
2024-09-26 17:19:26 -0700

    add indel tests

--- tests/testthat/test-strand-flip-handling.R
+++ tests/testthat/test-strand-flip-handling.R
@@ -28,10 +28,41 @@ test_that(
     'flip.DNA.allele correctly flips SNP alleles', {
 
         flipped.alleles <- c('T', 'A', 'G', 'C', NA);
-        names(flipped.alleles) <- c('A', 'T', 'C', 'G', NA);
         expect_equal(
             flip.DNA.allele(c('A', 'T', 'C', 'G', NA)),
             flipped.alleles
             );
         }
     );
+
+test_that(
+    'flip.DNA.allele correctly flips INDEL alleles', {
+
+        expect_equal(
+            flip.DNA.allele(c('ATCG', 'TATA', 'AAAA', 'TTTA', 'AACCTTGGAACCTTGG', NA)),
+            c('CGAT', 'TATA', 'TTTT', 'TAAA', 'CCAAGGTTCCAAGGTT', NA)
+            );
+        }
+    );
+
+# test data (single alleles):
+# 1. no strand flips
+# 2. effect allele switch
+# 3. strand flip
+# 4. palindromic (ambiguous) alleles
+
+vcf.ref.allele <- c('A', 'A', 'A', 'A')
+vcf.alt.allele <- c('G', 'G', 'G', 'T')
+pgs.ref.allele <- c('A', 'T', 'C', 'T')
+pgs.effect.allele <- c('G', 'C', 'T', 'A')
+
+# test data (multi-allelic):
+# 1. no strand flips
+# 2. effect allele switch
+# 3. strand flip
+# 4. palindromic (ambiguous) alleles
+
+vcf.ref.allele <- c('A', 'A', 'A', 'A')
+vcf.alt.allele <- c('G,T', 'G,T', 'G,T', 'T,C')
+pgs.ref.allele <- c('A', 'T', 'C', 'T')
+pgs.effect.allele <- c('G', 'C', 'T', 'A')
